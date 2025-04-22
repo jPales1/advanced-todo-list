d156e441feeddc7b080016c71454202ec9ca29f3
jpales1 <joaopedropales@gmail.com>
2025-04-22 17:11:42 -0300

    adicionar componentes TaskList e Welcome para exibir tarefas e mensagem de boas-vindas

---
+++ imports/ui/TaskList.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const TaskList = () => (
+  <div>
+    <h1>Lista de Tarefas</h1>
+    <p>Aqui você verá todas as suas tarefas.</p>
+  </div>
+);
---
+++ imports/ui/Welcome.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const Welcome = () => (
+  <div>
+    <h1>Bem-vindo ao Advanced ToDo List!</h1>
+    <p>Gerencie suas tarefas de forma eficiente.</p>
+  </div>
+);
