6edfcea639f7cd3ec53d5e0d9dbdc9da491f1648
jpales1 <joaopedropales@gmail.com>
2025-04-22 17:11:56 -0300

    refatorar componente App para usar roteamento e remover lógica de renderização condicional

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,20 +1,27 @@
 import { Meteor } from 'meteor/meteor';
-import React, { Fragment } from 'react';
+import React from 'react';
 import { useTracker } from "meteor/react-meteor-data";
 import { LoginForm } from './LoginForm';
+import { Welcome } from './Welcome';
+import { TaskList } from './TaskList';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 export const App = () => {
   const user = useTracker(() => Meteor.user());
 
+  if (!user) {
+    return <LoginForm />;
+  }
+
   return (
     <div className='main'>
-      {user ? (
-        <Fragment>
-          <h1>Seja bem-vindo ao Advanced ToDo List!</h1>
-        </Fragment>
-      ) : (
-        <LoginForm />
-      )}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/tasks" element={<TaskList />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Router>
     </div>
-  )
+  );
 };
