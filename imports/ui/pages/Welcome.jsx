db2a04c04f8a74c2dfe5b0852b99837e7dccaa14
jpales1 <joaopedropales@gmail.com>
2025-04-23 16:38:52 -0300

    refatorar página de boas-vindas para usar componentes do Material-UI, adicionando estilização semelhante à TaskList

--- imports/ui/pages/Welcome.jsx
+++ imports/ui/pages/Welcome.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from '@mui/material';
+import { Button, Container, Typography } from '@mui/material';
 
 export const Welcome = () => (
-  <div>
-    <h1>Bem-vindo ao Advanced ToDo List!</h1>
-    <p>Gerencie suas tarefas de forma eficiente.</p>
-    <Link to="/tasks">
+  <Container maxWidth="sm" style={{ textAlign: 'center', marginTop: '50px' }}>
+    <Typography variant="h3" gutterBottom>
+      Bem-vindo ao Advanced ToDo List!
+    </Typography>
+    <Typography variant="body1" component={"p"} gutterBottom>
+      Gerencie suas tarefas de forma eficiente.
+    </Typography>
+    <Link to="/tasks" style={{ textDecoration: 'none' }}>
       <Button variant="contained" color="primary">
         Ir para a lista de tarefas
       </Button>
     </Link>
-  </div>
+  </Container>
 );
