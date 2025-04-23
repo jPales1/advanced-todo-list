6b41dfba39d74eba325016e1eb1a02dcecbba675
jpales1 <joaopedropales@gmail.com>
2025-04-23 16:35:58 -0300

    adicionar link para a lista de tarefas na página de boas-vindas

--- imports/ui/pages/Welcome.jsx
+++ imports/ui/pages/Welcome.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from '@mui/material';
 
 export const Welcome = () => (
   <div>
     <h1>Bem-vindo ao Advanced ToDo List!</h1>
     <p>Gerencie suas tarefas de forma eficiente.</p>
+    <Link to="/tasks">
+      <Button variant="contained" color="primary">
+        Ir para a lista de tarefas
+      </Button>
+    </Link>
   </div>
 );
