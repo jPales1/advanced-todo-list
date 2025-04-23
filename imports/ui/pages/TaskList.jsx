be26d36e1fa8dd50de0fcfb2e1c81b1777dfe488
jpales1 <joaopedropales@gmail.com>
2025-04-23 15:58:45 -0300

    tarefa 3) refatorar componente TaskList para exibir lista de tarefas com componente List, exibindo ícone, nome da tarefa e nome do usuário que criou a tarefa

--- imports/ui/pages/TaskList.jsx
+++ imports/ui/pages/TaskList.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
+import { List, ListItem, ListItemIcon, ListItemText, Avatar, Typography, Container } from '@mui/material';
+import Fastfood from '@mui/icons-material/Fastfood';
+import { useTracker } from 'meteor/react-meteor-data';
 
-export const TaskList = () => (
-  <div>
-    <h1>Lista de Tarefas</h1>
-    <p>Aqui você verá todas as suas tarefas.</p>
-  </div>
-);
+export const TaskList = () => {
+  // Simulação de dados de tarefas
+  const tasks = useTracker(() => [
+    { id: 1, name: 'Comprar Hamburguér', createdBy: 'João' },
+    { id: 2, name: 'Comprar Milkshake', createdBy: 'Maria' },
+    { id: 3, name: 'Comprar Batata-Frita', createdBy: 'Carlos' },
+  ]);
+
+  return (
+    <Container maxWidth="sm" style={{ marginTop: '20px' }}>
+      <Typography variant="h4" gutterBottom>
+        Lista de Tarefas
+      </Typography>
+      <List>
+        {tasks.map((task) => (
+          <ListItem key={task.id}>
+            <ListItemIcon>
+              <Fastfood />
+            </ListItemIcon>
+            <ListItemText
+              primary={task.name}
+              secondary={`Criado por: ${task.createdBy}`}
+            />
+          </ListItem>
+        ))}
+      </List>
+    </Container>
+  );
+};
