96b0f2d016d5ea08ef47ef5ec455763988ffb77e
jpales1 <joaopedropales@gmail.com>
2025-05-15 16:22:24 -0300

    adicionar suporte ao tema personalizado e aplicar estilos globais com ThemeProvider e CssBaseline

--- client/main.jsx
+++ client/main.jsx
@@ -2,9 +2,30 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Meteor } from 'meteor/meteor';
 import { App } from '/imports/ui/App';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#8931D1',
+    },
+  },
+  typography: {
+    fontFamily: [
+      'Geist',
+      'sans-serif',
+    ].join(','),
+  },
+});
 
 Meteor.startup(() => {
   const container = document.getElementById('react-target');
   const root = createRoot(container);
-  root.render(<App />);
+  root.render(
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  );
 });
